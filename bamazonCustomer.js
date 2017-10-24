6427986d845216929b8210d5cb8acdbdde2aad7f
Heather <hlmathies@gmail.com>
2017-10-23 21:25:35 -0400

    create connection

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,16 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: 'root',
+  password:'',
+  database: 'bamazon'
+});
+
+connection.connect(function(err){
+  if (err) throw err;
+  console.log('you\'re connected')
+
+});
