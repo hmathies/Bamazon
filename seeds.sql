df2b6895c60c07ca3297024dac8398eb084c066a
Heather <hlmathies@gmail.com>
2017-10-23 21:09:42 -0400

    populate table with mock data

---
+++ seeds.sql
@@ -0,0 +1,31 @@
+USE bamazon;
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('wisk', 'cooking', 3.50, 100);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('shampoo', 'beauty', 2.75, 100);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('conditioner', 'beauty', 3.00, 100);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('spatula', 'cooking', 6.99, 100);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('shaving cream', 'beauty', 3.95, 100);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('olive oil', 'cooking', 4.99, 100);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('perfume', 'beauty', 39.99, 100);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('batteries', 'electronics', 9.99, 100);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('sunscreen', 'beauty', 2.99, 100);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('extension cord', 'electronics', 5.95, 100);
